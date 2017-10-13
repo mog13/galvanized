85edc391edf58a9575f740455491cdc9b2ec6c20
pbootly <mthomas@polybit.io>
2017-10-13 01:52:48 +0100

    Add gitignore and two core files/structures to build functions
    + Add retrieval.js which takes nodeName (called externally as it's an export)
      and gets the information throughout.
    + Add basic json configuration file similar to alternative config tools.
    
    TODO (Immediate):
    - Write ssh function and matrix to determine what command to run depending on OS
    - Investigate an ssh-agent for key choice, then determine if key is available

---
+++ utils/retrieval.js
@@ -0,0 +1,20 @@
+/*
+* TODO: Function that takes a node key as argument, reads through
+* nodes.json and gets configuration depending on device OS object.
+*/
+
+const nodeConfig = require('../config/nodes.json');
+
+let getConfig = function(nodeName){
+
+    // Traverse node configuration file, assigning nodeName to root of key.
+    for (let nodeName in nodeConfig){
+        let nodeInfo = nodeConfig[nodeName];
+        let ip = nodeInfo.ip;
+        let username = nodeInfo.username;
+        let password = nodeInfo.password;
+        let os = nodeInfo.os;
+    }
+};
+
+module.exports = getConfig;
